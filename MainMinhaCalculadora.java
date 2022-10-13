8a351cee9eb5c9e264b4ae9ace01a0d46f2bdf83
Isadora Rocha <115477897+is-Isadora-Rocha@users.noreply.github.com>
2022-10-13 11:56:23 -0300

    Add files via upload

---
+++ MainMinhaCalculadora.java
@@ -0,0 +1,14 @@
+package modificadorStatico;
+
+public class MainMinhaCalculadora {
+    public static void main(String[] args) {
+        //MinhaCalculadora calculadora = new MinhaCalculadora;
+        int soma1 = MinhaCalculadora.soma(1,2);
+        System.out.println(soma1);
+
+        int soma2 = MinhaCalculadora.soma(1,2,3);
+        System.out.println(soma2);
+    }
+
+
+}
